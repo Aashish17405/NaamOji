95af57e07b96ebab89976d3b61587057663b0664
Aashish17405 <aashish17405@gmail.com>
2025-02-25 23:34:24 +0530

    cors set for protection

--- server/app.js
+++ server/app.js
@@ -7,7 +7,17 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+const allowedOrigins = [process.env.VITE_APP_URL, 'http://localhost:5173'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json());
 app.use(actionRoutes);
 
