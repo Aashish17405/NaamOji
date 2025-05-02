edab0bce5c917819256969a5d2daf579662f01d1
Aashish Jaini <140961189+Aashish17405@users.noreply.github.com>
2025-05-02 16:13:45 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,15 +17,7 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = ['https://naamoji.dev-aashish.tech/', 'http://localhost:5173'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors());
 // app.use(limiter);
 app.use(express.json());
 app.use(actionRoutes);
